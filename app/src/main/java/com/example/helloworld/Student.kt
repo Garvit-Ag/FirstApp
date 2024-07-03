bf37e6d5d6485a3b6ebcc188ee4451861a9b4cba
Garvit-Ag <garvit.agarwal1706@gmail.com>
2024-07-03 17:49:23 +0530

    Kotlin is concise

--- app/src/main/java/com/example/helloworld/Student.kt
+++ app/src/main/java/com/example/helloworld/Student.kt
@@ -1,37 +1,3 @@
-package com.example.helloworld;
+package com.example.helloworld
 
-public class Student {
-    String name;
-    int age;
-    String address;
-
-    public Student(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+class Student(var name: String, var age: Int, var address: String)
--- app/src/main/java/com/example/helloworld/kotlinegs/Mydemo.kt
+++ app/src/main/java/com/example/helloworld/kotlinegs/Mydemo.kt
@@ -1,5 +1,9 @@
 package com.example.helloworld.kotlinegs
 
+import com.example.helloworld.Student
+
 fun main() {
     println("Hello World")
+    var obj: Student = Student("Garvit", 21,"Rajasthan")
+    println(obj.name)
 }
